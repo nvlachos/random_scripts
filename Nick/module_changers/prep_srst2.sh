92c3444bb53587c178c35f8e7b7b2d775aa24b07
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-10-29 08:50:40 -0400

    corrected prep srst2

---
+++ Nick/module_changers/prep_srst2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+#$ -o out_srst2.out
+#$ -e out_srst2.err
+#$ -N out_srst2
+#$ -cwd
+#$ -q all.q
+
+# unloads the loaded modules of python, bowtie2, and samtools for srst2 that were necessary for srst2 to work correctly
+# Wouldnt unload normally so had to make this file...sorry
+
+module unload Python/2.7
+module unload python/3.5.2
+module unload perl/5.22.1
+
+module load Python/2.7.15
+module load bowtie2/2.2.4
+module load samtools/0.1.18
+module load perl/5.16.1-MT
+module load srst2
