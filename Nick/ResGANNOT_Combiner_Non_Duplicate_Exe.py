789e0b3a9c1388a1665bdf8a37cfa3766d78c513
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-01 13:38:43 -0400

    fmty,k

--- Nick/ResGANNOT_Combiner_Non_Duplicate_Exe.py
+++ Nick/ResGANNOT_Combiner_Non_Duplicate_Exe.py
@@ -34,6 +34,7 @@ def Duplicate_Gene_Remover(input_fasta, output_file, output_copy_file):
         for genes_2 in range(len(Gene_List_out)):
             if str(Gene_List[genes].seq) == str(Gene_List_out[genes_2].seq):
                 Gene_Adder = 1
+                print("Wrong1")
                 Gene_copies_out.append(Gene_List[genes].description)
                 Gene_copies_out.append(Gene_List[genes].seq)
                 Gene_copies_out.append(Gene_List_out[genes_2].description)
@@ -41,6 +42,7 @@ def Duplicate_Gene_Remover(input_fasta, output_file, output_copy_file):
                 break
             elif str(Gene_List[genes].seq.reverse_complement()) == str(Gene_List_out[genes_2].seq):
                 Gene_Adder = 1
+                print("Wrong2")
                 Gene_copies_out.append(Gene_List[genes].description)
                 Gene_copies_out.append(Gene_List[genes].seq)
                 Gene_copies_out.append(Gene_List_out[genes_2].description)
