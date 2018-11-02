29a48f6bf368aaed9c0f27cec44cbbf6c4161d40
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-02 14:49:26 -0400

    fhtyj

--- Nick/ANI_test.sh
+++ Nick/ANI_test.sh
@@ -60,11 +60,11 @@ me=$(whoami)
 
 #mash dist "${local_DBs}/aniDB/refseq.genomes.k21s1000.msh" "${OUTDATADIR}/Assembly/${1}_scaffolds_trimmed.fasta" > "${OUTDATADIR}/ANI/${1}_all_refSeq.dists"
 counter=0
-for ref in ${local_DBs}/aniDB/all_test/*.fna; do
+for ref in ${local_DBs}/aniDB/all/*.fna; do
 	echo ${ref}
 	counter=$(( counter + 1 ))
 	filename=$(basename ${ref} | cut -d'_' -f1,2)
-	mash dist "${local_DBs}/aniDB/all_test/all_named.msh" "${ref}" > "${OUTDATADIR}/all_test/dists/${filename}_unsorted.dists"
+	mash dist "${local_DBs}/aniDB/all/all_named.msh" "${ref}" > "${OUTDATADIR}/all_test/dists/${filename}_unsorted.dists"
 	sort -k3 -n -o "${OUTDATADIR}/all_test/dists/${filename}.dists" "${OUTDATADIR}/all_test/dists/${filename}_unsorted.dists"
 	rm -r "${OUTDATADIR}/all_test/dists/${filename}_unsorted.dists"
 done
