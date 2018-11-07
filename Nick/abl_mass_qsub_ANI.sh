deeae2723b78855b9cb9ccd6d6d3dd781c996787
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-07 10:08:06 -0500

    ihrjbntg

--- Nick/abl_mass_qsub_ANI.sh
+++ Nick/abl_mass_qsub_ANI.sh
@@ -163,5 +163,5 @@ done
 echo "All isolates completed"
 global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
 #Script exited gracefully (unless something else inside failed)
-printf "%s %s" "Act_by_list.sh has completed ${2}" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
+#printf "%s %s" "Act_by_list.sh has completed ${2}" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
 exit 0
