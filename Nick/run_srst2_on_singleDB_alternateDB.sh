86b8faf730680c5039daf2cd4f0338b565114e72
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-09 11:16:59 -0500

    Testing _srst2

--- Nick/run_srst2_on_singleDB_alternateDB.sh
+++ Nick/run_srst2_on_singleDB_alternateDB.sh
@@ -34,9 +34,12 @@ fi
 alt_DB_path=${3}
 alt_DB=$(basename "${alt_DB_path}")
 alt_DB=$(echo ${alt_DB_path##*/} | cut -d'.' -f1)
+alt_DB=${alt_DB/_srst2//}
 
 echo ${alt_DB_path}
 echo ${alt_DB}
+
+exit
 
 mkdir "${processed}/${2}/${1}/srst2"
 
