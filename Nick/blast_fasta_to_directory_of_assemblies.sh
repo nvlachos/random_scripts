b02d4a4798fc4889ff1ce1fe40e7d2940a9b626c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-21 10:41:14 -0500

    Added blast directory

---
+++ Nick/blast_fasta_to_directory_of_assemblies.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -l
+
+#$ -o act_by_list_barebones.out
+#$ -e act_by_list_barebones.err
+#$ -N ablb
+#$ -cwd
+#$ -q all.q
+
+#Import the config file with shortcuts and settings
+. ./config.sh
+#Import the module file that loads all necessary mods
+. "${mod_changers}/pipeline_mods"
+
+#
+# Usage ./act_by_list.sh list_name(currently has to be placed in /scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR folder)
+#
+# script changes depending on what needs to be run through the list
+#
+
+# Checks for proper argumentation
+if [[ $# -eq 0 ]]; then
+	echo "No argument supplied to blast_list.sh, exiting"
+	exit 1
+# Shows a brief uasge/help section if -h option used as first argument
+elif [[ "$1" = "-h" ]]; then
+	echo "Usage is ./blast_fasta_to_directory_of_assemblies.sh input_folder absolute_path_to_fasta"
+	echo "Output location varies depending on which tasks are performed but will be found somewhere under ${processed}"
+	exit 0
+fi
+
+# Loop through and act on each sample name in the passed/provided list
+
+makeblastdb -in ${3} -out ${3} -dbtype nucl
+
+if [[ ! -d ${2} ]]; then
+	echo "Directory of fastas (${2}) don't exist... exiting"
+fi
+
+for assembly in ${2}/*;
+do
+	if [[ "${assembly}" = *".fasta"]]; then
+			sample_name=$(basename ${assembly})
+
+			blastOut=${2}/${sample_name}.blast
+			blastn -query ${assembly} -db ${3} -out $blastOut -word_size 10 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen"
+
+			echo "completed ${assembly} in loop at ${current_time}"
+done
+
+echo "All runs completed"
+global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
+#Script exited gracefully (unless something else inside failed)
+printf "%s %s" "Act_by_list.sh has completed whatever it was doing at" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
+exit 0
