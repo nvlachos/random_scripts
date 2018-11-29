5ee9dddd6a25489461deedf61f8f7a7fb2b1c830
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-29 12:41:22 -0500

    thm rntr

--- Nick/rename_sample.sh
+++ Nick/rename_sample.sh
@@ -18,7 +18,7 @@ if [[ ! -z ${4} ]]; then
 fi
 new_name=${2}
 project=${3}
-echo "Test 1-${old_name}-${new_name}"
+echo "Test 1-${old_name}-${new_name}, filename changing"
 find /scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${old_name} -type f -name "*${old_name}*" | while read FILE ; do
   dirname=$(dirname $FILE)
 	filename=$(basename $FILE)
@@ -29,24 +29,17 @@ find /scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${old_na
 	echo "${newfile}"
     mv "${FILE}" "${dirname}/${newfile}"
 done
-
-folders=()
-index=0
-find /scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${old_name} -type d > "/scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/temp_folders.txt"
-echo "Test 2"
-find /scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${old_name} -type f -exec sed -i 's/${old_name}/${new_name}/g' {} +
-echo "Test 3"
-for (( idx=${index}-1 ; idx>=0 ; idx-- )) ; do
-	subpath=$(echo "${folders[${idx}]}" | cut -d'/' -f1-10)
-	sub_sample=$(echo "${folders[${idx}]}" | cut -d'/' -f11-)
-	#echo "sp-${subpath}"
-	#echo "ss-$sub_sample"
-    new_dir="${subpath}/${sub_sample/${old_name}/${new_name}}"
-	echo "NT-${new_dir}"
-	if [[ "${new_dir}" != "${folders[${idx}]}" ]]; then
-		mv "${folders[${idx}]}" "${new_dir}"
+echo "Test 2, deep dive"
+for thing in /scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${old_name}/*; do
+	if [[ -f ${thing} ]]; then
+		sed -i -- 's/${old_name}/${new_name}/g' ${thing}
+	elif [[ -d ${thing} ]]l then
+		if [[ "${thing}" = *"FASTQs"]]; then
+			echo "Skipping FASTQs deep dive"
+		fi
+		find ${thing} -type f -exec sed -i 's/${old_name}/${new_name}/g' {} +
 	else
-		echo "Destination is the same as the source"
+		echo "Thing (${thing}) is not file or directory"
 	fi
 done
 mv "/scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${old_name}" "/scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${project}/${new_name}"
