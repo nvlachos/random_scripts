4a3ad95b0aeaa8ac3354afcf582a95fc61c27a61
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-12-03 10:59:30 -0500

    added list modules

---
+++ Nick/module_changers/list_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+#$ -o list_mods.out
+#$ -e list_mods.err
+#$ -N list_mods
+#$ -cwd
+#$ -q all.q
+
+# Script to print loaded modules to ensure the proper environment for running
+# Wouldnt show mod list properly so made this file to run on cluster....sorry
+module list
