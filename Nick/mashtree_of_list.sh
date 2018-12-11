06fb8c7e0b9b29cc838c2c1a811c51e74d42ac0b
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-12-11 10:48:28 -0500

    Edited ways to run mashtree of samples

---
+++ Nick/mashtree_of_list.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -l
+
+#$ -o mashdist.out
+#$ -e mashdist.err
+#$ -N mashdist
+#$ -cwd
+#$ -q all.q
+
+#Import the config file with shortcuts and settings
+if [[ ! -f "./config.sh" ]]; then
+	cp config_template.sh config.sh
+fi
+. ./config.sh
+
+#
+# Script to create mashtree of specified isolates that were processed by Quaisar pipeline
+#
+# Usage ./mashtree_of_list.sh absolute_path_to_list absolute_output_directory tree_output_name
+#
+
+
+if [[ ! -d ${2} ]]; then
+	mkdir -p ${2}
+fi
+
+while IFS= read -r line || [[ "$line" ]];  do
+	sample_name=$(echo "${line}" | cut -d'/' -f2 | tr -d '[:space:]')
+	project=$(echo "${line}" | cut -d'/' -f1 | tr -d '[:space:]')
+	cp ${processed}/${project}/${sample_name}/Assembly/${sample_name}_scaffolds_trimmed.fasta ${2}
+done < ${1}
+
+cd ${2}
+mashtree.pl --numcpus ${procs} *.fasta --tempdir ${2}/temp > "${2}/${3}.dnd";
+
+exit
