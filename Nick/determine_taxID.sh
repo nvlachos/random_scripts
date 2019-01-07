c77fce6a394a65b30aa5fece9bbb2dfea5952c2e
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-01-07 11:30:33 -0500

    jgds

--- Nick/determine_taxID.sh
+++ Nick/determine_taxID.sh
@@ -54,7 +54,7 @@ if [[ -d "${processed}/${project}/${sample}/ANI" ]]; then
 	source="ANI"
 	echo "${source}"
 	if [[ -f "${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_All).txt" ]]; then
-		source_file="${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_All).txt")
+		source_file="${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_All).txt"
 	else
 		source_file=$(ls -t "${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered"* | head n -1)
 	fi
