d00c6325049692aec40c64f5d267763aaeb20c3c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-01-16 16:20:07 -0500

    rstbrsrsr

--- Nick/determine_taxID.sh
+++ Nick/determine_taxID.sh
@@ -48,45 +48,98 @@ Order="Not_assigned"
 Family="Not_assigned"
 Genus="Not_assigned"
 species="Not_assigned"
-ani_files=0
+source="Not_assigned"
+confidence_index="0"
+source_file="Not_assigned"
 
-for f in ${processed}/${project}/${sample}/ANI/*; do
-	if [[ "${f}" = *"best_ANI_hits_ordered"* ]]; then
-    ani_files=1
-    break
+
+Check_source() {
+	start_at="${1}"
+	if [[ "${start_at}" -le 1 ]]; then
+		for f in ${processed}/${project}/${sample}/ANI/*; do
+			if [[ "${f}" = *"best_ANI_hits_ordered"* ]]; then
+				header=$(head -n1 ${f})
+				if [[ ${header} != "No matching ANI database found for"* ]]; then
+		    	do_ANI
+		    	return
+				fi
+			fi
+		done
 	fi
-done
-echo "${ani_files}"
+	if [[ "${start_at}" -le 2 ]]; then
+		if [[ -s "${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt" ]]; then
+			best_line=$(head -n1 "${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt")
+			largest_line=$(tail -n1 "${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt")
+			IFS='	' read -r -a best_array <<< "$best_line"
+			IFS='	' read -r -a largest_array <<< "$largest_line"
+			best_arr_size="${#best_array[@]}"
+			largest_arr_size="${#largest_array[@]}"
+			best_species=$(echo ${best_line} | cut -d'	' -f3)
+			largest_species=$(echo ${largest_line} | cut -d'	' -f3)
+			#echo "largest:${largest_species}:"
+			#echo "best:${best_species}:"
+			if [[ "${largest_arr_size}" -ge 3 ]]; then
+				do_16s "largest"
+				return
+			elif [[ "${best_arr_size}" -ge 3 ]]; then
+				do_16s "best"
+				return
+			fi
+		fi
+	fi
+	if [[ "${start_at}" -le 3 ]];then
+		if [[ -s "${processed}/${project}/${sample}/gottcha/${sample}_gottcha_species_summary.txt" ]]; then
+			do_GOTTCHA
+			return
+		fi
+	fi
+	if [[ "${start_at}" -le 4 ]]; then
+		if [[ -s "${processed}/${project}/${sample}/kraken/postAssembly/${sample}_kraken_summary_assembled_BP_data.txt" ]]; then
+			do_Kraken
+		return
+		fi
+	fi
+	echo "No ACCEPTABLE taxonomy file found to use"
+}
 
-if [[ ${ani_files} -gt 0 ]]; then
+do_ANI() {
 	source="ANI"
-	echo "${source}"
+	#echo "${source}"
 	if [[ -f "${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_All).txt" ]]; then
 		source_file="${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_All).txt"
 	else
 		source_file=$(ls -t "${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered"* | head -n 1)
 	fi
 	header=$(head -n 1 "${source_file}")
-	echo "${header}"
+	#echo "${header}"
 	Genus=$(echo "${header}" | cut -d' ' -f1 | cut -d'-' -f2)
 	species=$(echo "${header}" | cut -d' ' -f2 | cut -d'(' -f1)
-	confidence_index=$(echo "${header}" | cut -d' ' -f1 | cut -d'-' -f1)
-	echo "${Genus}-${species}"
-elif [[ -s "${processed}/${project}/${sample}/16s/${sample}.nt.RemoteBLASTN.sorted" ]]; then
-	source="16s"
-	#echo "${source}"
-		# Lookup Taxonomy
+	confidence_index=$(echo "${header}" | cut -d' ' -f1 | cut -d'%' -f1)
+	#echo "${Genus}-${species}"
+}
+
+do_16s() {
+	if [[ "${1}" = "largest" ]]; then
+		source_file="${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt"
 		line=$(tail -n 1 "${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt")
-		Genus=$(echo "${line}" | cut -d"	" -f3 | cut -d" " -f1)
-		species=$(echo "${line}" | cut -d"	" -f3 | cut -d" " -f2)
-		source_file="${processed}/${project}/${sample}/16s/${sample}.nt.RemoteBLASTN.sorted"
-		confidence_index=$(head -n1 ${source_file} | cut -d'	' -f3)
-		confidence_index="${confidence_index}%"
-elif [[ -s "${processed}/${project}/${sample}/gottcha/${sample}_gottcha_species_summary.txt" ]]; then
+		source="16s_largest"
+		confidence_index=$(head -n1 "${processed}/${project}/${sample}/16s/${sample}.nt.RemoteBLASTN.sorted" | cut -d'	' -f3)
+		confidence_index="${confidence_index}"
+	elif [[ "${1}" = "best" ]]; then
+		line=$(head -n 1 "${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt")
+		source="16s_best"
+		confidence_index=$(head -n1 "${processed}/${project}/${sample}/16s/${sample}.nt.RemoteBLASTN" | cut -d'	' -f3)
+		confidence_index="${confidence_index}"
+	else
+		break
+	fi
+	Genus=$(echo "${line}" | cut -d"	" -f3 | cut -d" " -f1)
+	species=$(echo "${line}" | cut -d"	" -f3 | cut -d" " -f2)
+}
+
+do_GOTTCHA() {
 	source="GOTTCHA"
 	source_file="${processed}/${project}/${sample}/gottcha/${sample}_gottcha_species_summary.txt"
-	confidence_index=$(tail -n1 "${source_file}" | cut -d' ' -f2)
-	confidence_index="${confidence_index}%"
 	#echo "${source}"
 	while IFS= read -r line;
 	do
@@ -100,12 +153,14 @@ elif [[ -s "${processed}/${project}/${sample}/gottcha/${sample}_gottcha_species_
 		then
 			Genus=$(echo "${line}" | awk -F ' ' '{print $4}')
 		fi
+		confidence_index=$(tail -n1 "${source_file}" | cut -d' ' -f2)
+		confidence_index="${confidence_index}"
 	done < "${processed}/${project}/${sample}/gottcha/${sample}_gottcha_species_summary.txt"
-elif [[ -s "${processed}/${project}/${sample}/kraken/postAssembly/${sample}_kraken_summary_assembled_BP_data.txt" ]]; then
+}
+
+do_Kraken() {
 	source="Kraken"
 	source_file="${processed}/${project}/${sample}/kraken/postAssembly/${sample}_kraken_summary_assembled_BP_data.txt"
-	confidence_index=$(tail -n1 "${source_file}" | cut -d' ' -f2)
-	confidence_index="${confidence_index}%"
 	#echo "${source}"
 	while IFS= read -r line;
 	do
@@ -120,9 +175,17 @@ elif [[ -s "${processed}/${project}/${sample}/kraken/postAssembly/${sample}_krak
 			Genus=$(echo "${line}" | awk -F ' ' '{print $4}')
 		fi
 	done < "${processed}/${project}/${sample}/kraken/postAssembly/${sample}_kraken_summary_assembled_BP_data.txt"
-else
-	echo "Exiting, no reliable taxonomy files exist"
-	exit 1
+	confidence_index=$(tail -n1 "${source_file}" | cut -d' ' -f2)
+	confidence_index="${confidence_index}"
+}
+
+Check_source 0
+
+if [[ ! -z ${Genus} ]]; then
+	Genus=$(echo ${Genus} | tr -d [:space:])
+fi
+if [[ ! -z ${species} ]]; then
+	species=$(echo ${species} | tr -d [:space:])
 fi
 
 if [[ ${Genus} == "Peptoclostridium" ]]; then
@@ -144,4 +207,5 @@ do
 			break
 	fi
 done < "${local_DBs}/taxes.csv"
-echo -e "(${source})-${confidence_index}-${source_file}\nD:	${Domain}\nP:	${Phylum}\nC:	${Class}\nO:	${Order}\nF:	${Family}\nG:	${Genus}\ns:	${species}\n" > "${processed}/${project}/${sample}/${sample}.tax"
+
+printf "(${source})-${confidence_index}%%-${source_file}\nD:	${Domain}\nP:	${Phylum}\nC:	${Class}\nO:	${Order}\nF:	${Family}\nG:	${Genus}\ns:	${species}\n" > "${processed}/${project}/${sample}/${sample}.tax"
