ff7b3cb395f40d1f11df59a53fa08d9d009fa602
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-01-31 14:10:18 -0500

    jyrd

--- Nick/rename_project.sh
+++ Nick/rename_project.sh
@@ -14,52 +14,5 @@ fi
 
 new_project_name=${2}
 old_project_name=${1}
-
-
-# Finding all internal instances of old project name and changing them to new preoject name
-echo "Testing new internal finder"
-#find . -not -name "*.fq" -not -name "*.fastq" -not -name "*.fsq" -not -name "*.fasta" -type f -print0 | xargs -0 sed -i 's/${old_project_name}/${new_project_name}/g'
-find "${processed}/${old_project_name}/" -type f -not -path "*FASTQs*" -not -path "*removedAdapters" -not -path "*trimmed" -not -path "*ANI" -print0  {} \; #sed -i "s/${old_project_name}/${new_sample_name}/g"
-#grep -lr --exclude-dir="removedAdapters" --exclude-dir="trimmed" --exclude-dir="FASTQs" --exclude-dir="ANI" -e "${old_project_name}" ${processed}/${old_project_name} | xargs sed -i '' -e 's/${old_project_name}/${new_project_name}/g'
-#grep -lr --exclude-dir="removedAdapters" --exclude-dir="trimmed" --exclude-dir="FASTQs" --exclude-dir="ANI" -e "${old_project_name}" ${processed}/${old_project_name} > ${processed}/${old_project_name}/groutput.txt
-
-# for thing in ${processed}/${old_project_name}/*; do
-# 	if [[ -d ${thing} ]]; then
-# 		if [[ "${thing}" = *"FASTQs" ]] || [[ "${thing}" = *"removedAdapters" ]] || [[ "${thing}" = *"trimmed" ]]; then
-# 			continue
-# 		else
-# 			echo "Trying ${thing}"
-# 			find ${thing} -type f -exec sed -i 's/${old_project_name}/${new_sample_name}/g' {} +
-# 		fi
-# 	fi
-# done
-
-#echo "Test 1, Searching contents of files"
-#for thing in /scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/${old_project_name}/*; do
-#	if [[ -f ${thing} ]]; then
-#		echo "Doing normal - $thing"
-#	elif [[ -d ${thing} ]]; then
-#		echo "doing directory - $thing"
-#		if [[ "${thing}" = *"FASTQs" ]]; then
-#			echo "Skipping FASTQs deep dive"
-#		fi
-#		find ${thing} -type f -exec sed -i "s/${old_project_name}/${new_project_name}/g" {} +
-#	else
-#		echo "Thing (${thing}) is not file or directory"
-#	fi
-#done
-
-
-# Changing all files names containing old project name
-echo "Testing new filename changer"
-
-mv "${processed}/${old_project_name}" "/${processed}/${new_project_name}"
-
-
-for i in ${processed}/${new_project_name}/* ;do mv -- "$i" "${i//${old_project_name}/${new_project_name}}";done
-
-#for thing in ${processed}/${old_project_name}/*; do
-#	if [[ "${thing}" = *"${old_project_name}"* ]]; then
-#		rename ${old_project_name} ${new_project_name} ${thing}
-#	fi
-#done
+directory_to_scan=${3}
+ python "${shareScript}/rename_project.py" ${1} ${2} ${3}
