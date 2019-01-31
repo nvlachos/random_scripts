e9effc4d2020206c3926da4bc9518e87d99ea14e
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-01-31 10:54:47 -0500

    etdrftgyhuji

--- Nick/rename_project.sh
+++ Nick/rename_project.sh
@@ -20,8 +20,8 @@ old_project_name=${1}
 echo "Testing new internal finder"
 #find . -not -name "*.fq" -not -name "*.fastq" -not -name "*.fsq" -not -name "*.fasta" -type f -print0 | xargs -0 sed -i 's/${old_project_name}/${new_project_name}/g'
 
-#grep -lr --exclude-dir="removedAdapters" --exclude-dir="trimmed" --exclude-dir="FASTQs" --exclude-dir="ANI" -e "${old_project_name}" ${processed}/${old_project_name} | xargs sed -i '' -e 's/${old_project_name}/${new_project_name}/g'
-grep -lr --exclude-dir="removedAdapters" --exclude-dir="trimmed" --exclude-dir="FASTQs" --exclude-dir="ANI" -e "${old_project_name}" ${processed}/${old_project_name} > ${processed}/${old_project_name}/groutput.txt
+grep -lr --exclude-dir="removedAdapters" --exclude-dir="trimmed" --exclude-dir="FASTQs" --exclude-dir="ANI" -e "${old_project_name}" ${processed}/${old_project_name} | xargs sed -i '' -e 's/${old_project_name}/${new_project_name}/g'
+#grep -lr --exclude-dir="removedAdapters" --exclude-dir="trimmed" --exclude-dir="FASTQs" --exclude-dir="ANI" -e "${old_project_name}" ${processed}/${old_project_name} > ${processed}/${old_project_name}/groutput.txt
 
 # for thing in ${processed}/${old_project_name}/*; do
 # 	if [[ -d ${thing} ]]; then
