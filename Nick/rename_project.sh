c70e08661ff6b1952481e436f2f01f08cb0bf74a
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-02-22 10:46:31 -0500

    test for python 3

--- Nick/rename_project.sh
+++ Nick/rename_project.sh
@@ -9,10 +9,13 @@
 #Import the config file with shortcuts and settings
 . ./config.sh
 if [[ "$1" = "-h" ]]; then
-	echo "Usage is ./rename_sample.sh old_project_name new_project_name"
+	echo "Usage is ./rename_sample.sh old_project_name new_project_name directory_to_scan"
 fi
 
 new_project_name=${2}
 old_project_name=${1}
 directory_to_scan=${3}
- python3 "${shareScript}/rename_project.py" ${1} ${2} ${3}
+python -V
+python2 -V
+python3 -V
+#python3 "${shareScript}/rename_project.py" ${1} ${2} ${3}
