b2348d546552fb01ed7028f4cee6ee91ca2e6b72
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-06 11:47:36 -0500

    New update

--- Nick/abl_mass_qsub_alt_csstar.sh
+++ Nick/abl_mass_qsub_alt_csstar.sh
@@ -12,43 +12,57 @@
 . "${mod_changers}/pipeline_mods"
 
 #
-# Usage ./act_by_list.sh list_name(currently has to be placed in /scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR folder) description of list function
-#
-# script changes depending on what needs to be run through the list
+# Usage ./abl_mass_qsub_alt_csstar.sh path_to_list max_concurrent_submissions path_to_alt_database output_directory_for_scripts
 #
 
 # Checks for proper argumentation
 if [[ $# -eq 0 ]]; then
-	echo "No argument supplied to act_by_list.sh, exiting"
+	echo "No argument supplied to ./abl_mass_qsub_alt_csstar.sh, exiting"
 	exit 1
 # Shows a brief uasge/help section if -h option used as first argument
 elif [[ "$1" = "-h" ]]; then
-	echo "Usage is ./abl_mass_qsub_csstar_alt.sh path_to_list_file(single sample ID per line, e.g. B8VHY/1700128 (it must include project id also)) max_submissions path_to_alt_database"
-	echo "Output location varies depending on which tasks are performed but will be found somewhere under ${processed}"
-	exit 0
+	echo "Usage is ./abl_mass_qsub_alt_csstar.sh path_to_list_file(single sample ID per line, e.g. B8VHY/1700128 (it must include project id also)) max_concurrent_submissions path_to_alt_database output_directory_for_scripts"
+	exit 1
+elif [[ ! -f "${1}" ]]; then
+	echo "${1} (list) does not exist...exiting"
+	exit 1
+elif [[ ! -f "${3}" ]]; then
+	echo "${3} (alt_db) does not exist...exiting"
+	exit 1
 fi
 
+echo "1: ${1}
+	2:${2}
+	3:${3}
+	4:${4}
+	"
+
+
+# create an array of all samples in the list
 arr=()
 while IFS= read -r line || [[ "$line" ]];  do
   arr+=("$line")
 done < ${1}
 
 arr_size="${#arr[@]}"
+last_index=$(( arr_size -1 ))
 echo "-${arr_size}:${arr[@]}-"
 
 
-# Loop through and act on each sample name in the passed/provided list
+# Create direcory to hold all temporary qsub scripts
 counter=0
 max_subs=${2}
-main_dir="${share}/mass_subs/csstar_alt_subs"
-if [[ ! -d "${share}/mass_subs/csstar_alt_subs" ]]; then
-	mkdir "${share}/mass_subs/csstar_alt_subs"
-	mkdir "${share}/mass_subs/csstar_alt_subs/complete"
-elif [[ ! -d  "${share}/mass_subs/csstar_alt_subs/complete" ]]; then
-	mkdir "${share}/mass_subs/csstar_alt_subs/complete"
+
+# Set script directory
+main_dir="${4}/csstar_alt_subs"
+if [[ ! -d "${4}/csstar_alt_subs" ]]; then
+	mkdir "${4}/csstar_alt_subs"
+	mkdir "${4}/csstar_alt_subs/complete"
+elif [[ ! -d  "${4}/csstar_alt_subs/complete" ]]; then
+	mkdir "${4}/csstar_alt_subs/complete"
 fi
 
-
+# format name being extracted from alt database
 alt_database_path=$(basename -- "${3}")
 echo "${alt_database_path}"
 alt_database=$(echo ${alt_database_path##*/} | cut -d'.' -f1)
@@ -57,15 +71,12 @@ alt_database=${alt_database//_srst2/}
 echo "${alt_database}"
 
 
-start_time=$(date "+%F-%T")
+start_time=$(date "+%m-%d-%Y_at_%Hh_%Mm_%Ss")
 
+# Create and submit qsub file for each isolate in the list, pausing if sunbissions reach max_concurrent_submissions until space frees up
 while [ ${counter} -lt ${arr_size} ] ; do
 	sample=$(echo "${arr[${counter}]}" | cut -d'/' -f2)
 	project=$(echo "${arr[${counter}]}" | cut -d'/' -f1)
-	rm -r "${processed}/${project}/${sample}/c-sstar/alt_DB=_gapped/"
-	rm -r "${processed}/${project}/${sample}/c-sstar/${sample}.alt_DB=.gapped_98_sstar_summary.txt"
-	rm -r "${processed}/${project}/${sample}/c-sstar/${alt_database}_gapped/"
-	rm -r "${processed}/${project}/${sample}/c-sstar/${sample}.${alt_database}.gapped_98_sstar_summary.txt"
 	echo ${counter}
 	if [[ -s "${processed}/${project}/${sample}/Assembly/${sample}_scaffolds_trimmed.fasta" ]]; then
 		if [[ ${counter} -lt ${max_subs} ]]; then
@@ -83,7 +94,15 @@ while [ ${counter} -lt ${arr_size} ] ; do
 				# Defaulting to gapped/98, change if you want to include user preferences
 				echo -e "\"${shareScript}/run_c-sstar_on_single_alternate_DB.sh\" \"${sample}\" g h \"${project}\" \"${3}\"" >> "${main_dir}/csstn_${sample}_${start_time}.sh"
 				echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_csstarn_complete.txt\"" >> "${main_dir}/csstn_${sample}_${start_time}.sh"
-				qsub "${main_dir}/csstn_${sample}_${start_time}.sh"
+				if [[ "${counter}" -lt "${last_index}" ]]; then
+					qsub "${main_dir}/csstn_${sample}_${start_time}.sh"
+				else
+					if [[ -d "${processed}/${project}/${sample}/c-sstar_plasmid" ]]; then
+						qsub "${main_dir}/csstn_${sample}_${start_time}.sh"
+					else
+						qsub -sync y "${main_dir}/csstn_${sample}_${start_time}.sh"
+					fi
+				fi
 			else
 				echo "${project}/${sample} already has 0608"
 				echo "$(date)" > "${main_dir}/complete/${sample}_csstarn_complete.txt"
@@ -101,7 +120,11 @@ while [ ${counter} -lt ${arr_size} ] ; do
 					# Defaulting to gapped/98, change if you want to include user preferences
 					echo -e "\"${shareScript}/run_c-sstar_on_single_alternate_DB.sh\" \"${sample}\" g o \"${project}\" \"${3}\" \"--plasmid\"" >> "${main_dir}/csstp_${sample}_${start_time}.sh"
 					echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_csstarp_complete.txt\"" >> "${main_dir}/csstp_${sample}_${start_time}.sh"
-					qsub "${main_dir}/csstp_${sample}_${start_time}.sh"
+					if [[ "${counter}" -lt "${last_index}" ]]; then
+						qsub "${main_dir}/csstp_${sample}_${start_time}.sh"
+					else
+						qsub -sync y "${main_dir}/csstp_${sample}_${start_time}.sh"
+					fi
 					if [[ -f "${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_98_sstar_summary.txt" ]]; then
 						rm "${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_98_sstar_summary.txt"
 					fi
@@ -134,7 +157,15 @@ while [ ${counter} -lt ${arr_size} ] ; do
 						# Defaulting to gapped/98, change if you want to include user preferences
 						echo -e "\"${shareScript}/run_c-sstar_on_single_alternate_DB.sh\" \"${sample}\" g h \"${project}\" \"${3}\"" >> "${main_dir}/csstn_${sample}_${start_time}.sh"
 						echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_csstarn_complete.txt\"" >> "${main_dir}/csstn_${sample}_${start_time}.sh"
-						qsub "${main_dir}/csstn_${sample}_${start_time}.sh"
+						if [[ "${counter}" -lt "${last_index}" ]]; then
+							qsub "${main_dir}/csstn_${sample}_${start_time}.sh"
+						else
+							if [[ -d "${processed}/${project}/${sample}/c-sstar_plasmid" ]]; then
+								qsub "${main_dir}/csstn_${sample}_${start_time}.sh"
+							else
+								qsub -sync y "${main_dir}/csstn_${sample}_${start_time}.sh"
+							fi
+						fi
 					else
 						echo "${project}/${sample} already has 0608"
 						echo "$(date)" > "${main_dir}/complete/${sample}_csstarn_complete.txt"
@@ -152,7 +183,11 @@ while [ ${counter} -lt ${arr_size} ] ; do
 							# Defaulting to gapped/98, change if you want to include user preferences
 							echo -e "\"${shareScript}/run_c-sstar_on_single_alternate_DB.sh\" \"${sample}\" g o \"${project}\" \"${3}\" \"--plasmid\"" >> "${main_dir}/csstp_${sample}_${start_time}.sh"
 							echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_csstarp_complete.txt\"" >> "${main_dir}/csstp_${sample}_${start_time}.sh"
-							qsub "${main_dir}/csstp_${sample}_${start_time}.sh"
+							if [[ "${counter}" -lt "${last_index}" ]]; then
+								qsub "${main_dir}/csstp_${sample}_${start_time}.sh"
+							else
+								qsub -sync y "${main_dir}/csstp_${sample}_${start_time}.sh"
+							fi
 							if [[ -f "${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_98_sstar_summary.txt" ]]; then
 								rm "${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_98_sstar_summary.txt"
 							fi
@@ -176,7 +211,4 @@ while [ ${counter} -lt ${arr_size} ] ; do
 done
 
 echo "All isolates completed"
-global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
-#Script exited gracefully (unless something else inside failed)
-#printf "%s %s" "Act_by_list.sh has completed ${2}" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
 exit 0
