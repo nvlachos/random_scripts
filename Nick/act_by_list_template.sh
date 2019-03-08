d959a43a4665b4030f358bda76eb3cb58cce6435
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-08 14:17:39 -0500

    ktyf

--- Nick/act_by_list_template.sh
+++ Nick/act_by_list_template.sh
@@ -31,7 +31,7 @@ while IFS= read -r var; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
 	if [[ -f ${processed}/${project}/${sample_name}/srst2/${sample_name}_ResGANNOT_20180608__genes__ResGANNOT_20180608_srst2__results.txt ]]; then
-		mv ${processed}/${project}/${sample_name}/srst2/${sample_name}_ResGANNOT_20180608__genes__ResGANNOT_20180608_srst2__results.txt ${processed}/${project}/${sample_name}/srst2/${sample_name}__fullgenes__ResGANNOT_20180608_srst2__results.txt
+		mv ${processed}/${project}/${sample_name}/srst2/${sample_name}__genes__ResGANNOT_20180608_srst2__results.txt ${processed}/${project}/${sample_name}/srst2/${sample_name}__fullgenes__ResGANNOT_20180608_srst2__results.txt
 	fi
 done < "${1}"
 echo "All isolates completed"
