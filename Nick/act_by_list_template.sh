c0b07f9faa8c066d46de085ad3fa79d724ac7237
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-08 14:50:34 -0500

    kutfyjt

--- Nick/act_by_list_template.sh
+++ Nick/act_by_list_template.sh
@@ -30,7 +30,7 @@ counter=0
 while IFS= read -r var; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
-	if [[ -f ${processed}/${project}/${sample_name}/srst2/${sample_name}_ResGANNOT_20180608__genes__ResGANNOT_20180608_srst2__results.txt ]]; then
+	if [[ -f ${processed}/${project}/${sample_name}/srst2/${sample_name}__genes__ResGANNOT_20180608_srst2__results.txt ]]; then
 		mv ${processed}/${project}/${sample_name}/srst2/${sample_name}__genes__ResGANNOT_20180608_srst2__results.txt ${processed}/${project}/${sample_name}/srst2/${sample_name}__fullgenes__ResGANNOT_20180608_srst2__results.txt
 	fi
 done < "${1}"
