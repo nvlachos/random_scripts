456928170dfaace8c3b6829fce329404c5faa20c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-11 08:15:57 -0400

    fuyk,l;.

--- Nick/AR-updater.sh
+++ Nick/AR-updater.sh
@@ -35,6 +35,8 @@ fi
 
 # Checks the last time isolates were updated for AR. If databases havent changed since then, then no need to do any updating
 last_run=$(tail -n1 ${local_DBs}/ar_updater.txt)
+echo "Last time run=${last_run}
+ResGANNOT created=${resGANNOT_srst2_filename}"
 if [[ "${last_run}" = "${resGANNOT_srst2_filename}" ]]; then
 	echo "The last run was with the newest database... ${resGANNOT_srst2_filename}, exiting"
 	exit 0
@@ -47,7 +49,7 @@ resGANNOT_date=$(echo ${resGANNOT_srst2_filename} | cut -d'_' -f2)
 qsub "${shareScript}/abl_mass_qsub_csstar.sh" "${shareScript}/sample_list_${today}.txt" 100 "/scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/mass_subs"
 qsub -sync y "${shareScript}/abl_mass_qsub_srst2.sh" "${shareScript}/sample_list_${today}.txt" 100 "/scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/mass_subs"
 qsub "${shareScript}/abl_mass_qsub_runsum.sh" "${shareScript}/directory_list_${today}.txt" 100 "/scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/mass_subs"
-qsub -sync y "${shareScript}/act_by_list_AR_completion_check.sh" "${shareScript}/sample_list_${today}.txt" "${resGANNOT_date}" "/scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/AR_check_${today}"
+qsub -sync y "${shareScript}/act_by_list_AR_completion_check.sh" "${shareScript}/sample_list_${today}.txt" "${resGANNOT_date}" "/scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/AR_check_${today}.txt"
 
 
 
