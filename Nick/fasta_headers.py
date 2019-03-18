02d593145c6abf91b0dc45735e98a0106f4c718a
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-18 12:04:41 -0400

    eyj

--- Nick/fasta_headers.py
+++ Nick/fasta_headers.py
@@ -21,7 +21,7 @@ for record in SeqIO.parse(sys.argv[1],"fasta"):
     print(record.id)
     name=os.path.basename(sys.argv[1]).split("_")[::-1]
     name=name[3:]
-    name='_'.join(name[::])
+    name='_'.join(name[::-1])
     print(name)
     record.id = record.id.split("_cov")[0].replace("NODE",name)
     #print(record.id)
