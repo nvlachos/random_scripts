bc74245f94d9b4da4b96ed005df9a5133d247cd7
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-25 10:18:26 -0400

    Formatted to match structure of other abl_mass_qsub scripts.

--- Nick/abl_mass_qsub_alt_csstar.sh
+++ Nick/abl_mass_qsub_alt_csstar.sh
@@ -1,18 +1,20 @@
 #!/bin/sh -l
 
-#$ -o ablmq-csn.out
-#$ -e ablmq-csn.err
-#$ -N ablmq-csn
+#$ -o amq-altc.out
+#$ -e amq-altc.err
+#$ -N amq-altc
 #$ -cwd
 #$ -q short.q
 
 #Import the config file with shortcuts and settings
-. ./config.sh
+if [[ ! -f "./config.sh" ]]; then
+	cp ./config_template.sh ./config.sh
+fi
 #Import the module file that loads all necessary mods
 . "${mod_changers}/pipeline_mods"
 
 #
-# Usage ./abl_mass_qsub_alt_csstar.sh path_to_list max_concurrent_submissions path_to_alt_database output_directory_for_scripts
+# Usage ./abl_mass_qsub_alt_csstar.sh path_to_list max_concurrent_submissions path_to_alt_database output_directory_for_scripts cloberness (keep|clobber)
 #
 
 # Checks for proper argumentation
@@ -29,12 +31,24 @@ elif [[ ! -f "${1}" ]]; then
 elif [[ ! -f "${3}" ]]; then
 	echo "${3} (alt_db) does not exist...exiting"
 	exit 1
+elif [[ -z "${5}" ]] ||; then
+	echo "Clobberness was not input, be sure to add keep or clobber as 4th parameter...exiting"
+	exit 1
+fi
+
+# Check that clobberness is a valid option
+if [[ "${5}" != "keep" ]] && [[ "${5}" != "clobber" ]]; then
+	echo "Clobberness was not input, be sure to add keep or clobber as 4th parameter...exiting"
+	exit 1
+else
+	clobberness="${5}"
 fi
 
 echo "1: ${1}
 	2:${2}
 	3:${3}
 	4:${4}
+	5:${5}
 	"
 
 
@@ -75,13 +89,21 @@ start_time=$(date "+%m-%d-%Y_at_%Hh_%Mm_%Ss")
 
 # Create and submit qsub file for each isolate in the list, pausing if sunbissions reach max_concurrent_submissions until space frees up
 while [ ${counter} -lt ${arr_size} ] ; do
+	echo ${counter}
 	sample=$(echo "${arr[${counter}]}" | cut -d'/' -f2)
 	project=$(echo "${arr[${counter}]}" | cut -d'/' -f1)
-	echo ${counter}
+	# Delete old results if clobbering
+	if [[ "${5}" == "clobber" ]]; then
+		rm ${processed}/${project}/${sample}/c-sstar/${sample}.${alt_database}.gapped_98_sstar_summary
+		rm -r ${processed}/${project}/${sample}/c-sstar/${alt_database}_gapped/
+	fi
+
+	# Check if there is an assembly to use
 	if [[ -s "${processed}/${project}/${sample}/Assembly/${sample}_scaffolds_trimmed.fasta" ]]; then
+		# Check if counter has reached max submissions yet
 		if [[ ${counter} -lt ${max_subs} ]]; then
 			echo  "Index is below max submissions, submitting"
-
+			# Check if old results exist, and skip if so
 			if [[ ! -f "${processed}/${project}/${sample}/c-sstar/${sample}.${alt_database}.gapped_98_sstar_summary.txt" ]]; then
 				echo  "Index is below max submissions, submitting"
 				echo -e "#!/bin/bash -l\n" > "${main_dir}/csstn_${sample}_${start_time}.sh"
@@ -110,7 +132,14 @@ while [ ${counter} -lt ${arr_size} ] ; do
 				echo "$(date)" > "${main_dir}/complete/${sample}_csstarn_complete.txt"
 			fi
 
+			# Check if there is a plasmid folder to run also
 			if [[ -d "${processed}/${project}/${sample}/c-sstar_plasmid" ]]; then
+				# Delete old results if clobbering
+				if [[ "${5}" == "clobber" ]]; then
+					rm ${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_40_sstar_summary
+					rm -r ${processed}/${project}/${sample}/c-sstar_plasmid/${alt_database}_gapped/
+				fi
+				# Check if old results exist, and skip if so
 				if [[ ! -f "${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_40_sstar_summary.txt" ]]; then
 					echo -e "#!/bin/bash -l\n" > "${main_dir}/csstp_${sample}_${start_time}.sh"
 					echo -e "#$ -o csstp_${sample}.out" >> "${main_dir}/csstp_${sample}_${start_time}.sh"
@@ -137,6 +166,7 @@ while [ ${counter} -lt ${arr_size} ] ; do
 					echo "$(date)" > "${main_dir}/complete/${sample}_csstarp_complete.txt"
 				fi
 			fi
+		# Counter has exceeded max submissions limit
 		else
 			waiting_for_index=$(( counter - max_subs ))
 			waiting_sample=$(echo "${arr[${waiting_for_index}]}" | cut -d'/' -f2)
@@ -144,11 +174,19 @@ while [ ${counter} -lt ${arr_size} ] ; do
 			echo "Index is above max submissions, waiting for index ${waiting_for_index}:${waiting_sample} to complete"
 			while :
 			do
+				# Check to see if max time limit has been reached, exit if so.
 				if [[ ${timer} -gt 1800 ]]; then
 					echo "Timer exceeded limit of 1800 seconds 30 minutes"
 					break
 				fi
+				# Check if the sample it is waiting on has been completed or if it has a usable assembly
 				if [[ -f "${main_dir}/complete/${waiting_sample}_csstarn_complete.txt" ]] || [[ ! -s "${processed}/${project}/${waiting_sample}/Assembly/${waiting_sample}_scaffolds_trimmed.fasta" ]]; then
+					# Delete old if clobbering
+					if [[ "${5}" == "clobber" ]]; then
+						rm ${processed}/${project}/${sample}/c-sstar/${sample}.${alt_database}.gapped_98_sstar_summary
+						rm -r ${processed}/${project}/${sample}/c-sstar/${alt_database}_gapped/
+					fi
+					# Check if ol results exist and skip if so
 					if [[ ! -f "${processed}/${project}/${sample}/c-sstar/${sample}.${alt_database}.gapped_98_sstar_summary.txt" ]]; then
 						echo  "Index is below max submissions, submitting"
 						echo -e "#!/bin/bash -l\n" > "${main_dir}/csstn_${sample}_${start_time}.sh"
@@ -176,8 +214,14 @@ while [ ${counter} -lt ${arr_size} ] ; do
 						echo "${project}/${sample} already has 0608"
 						echo "$(date)" > "${main_dir}/complete/${sample}_csstarn_complete.txt"
 					fi
-
+					# Check if c-sstar plasmid folder exists
 					if [[ -d "${processed}/${project}/${sample}/c-sstar_plasmid" ]]; then
+						# Delete old results if clobbering
+						if [[ "${5}" == "clobber" ]]; then
+							rm ${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_40_sstar_summary
+							rm -r ${processed}/${project}/${sample}/c-sstar_plasmid/${alt_database}_gapped/
+						fi
+						# Check if data already exists and skip if so
 						if [[ ! -f "${processed}/${project}/${sample}/c-sstar_plasmid/${sample}.${alt_database}.gapped_40_sstar_summary.txt" ]]; then
 							echo -e "#!/bin/bash -l\n" > "${main_dir}/csstp_${sample}_${start_time}.sh"
 							echo -e "#$ -o csstp_${sample}.out" >> "${main_dir}/csstp_${sample}_${start_time}.sh"
@@ -205,6 +249,7 @@ while [ ${counter} -lt ${arr_size} ] ; do
 						fi
 					fi
 					break
+				# Wait 5 seconds and check if waiting sample is complete yet
 				else
 					timer=$(( timer + 5 ))
 					echo "sleeping for 5 seconds, so far slept for ${timer}"
@@ -212,11 +257,59 @@ while [ ${counter} -lt ${arr_size} ] ; do
 				fi
 			done
 		fi
+	# If no assembly file exists
 	else
 		echo "${project}/${sample} does not have an assembly file???"
 	fi
 	counter=$(( counter + 1 ))
 done
+
+# Loop to ensure all samples are complete (or time runs) before allowing the script to exit
+timer=0
+ptimer=0
+for item in "${arr[@]}"; do
+	waiting_sample=$(echo "${item}" | cut -d'/' -f2)
+	if [[ -f "${main_dir}/complete/${waiting_sample}_csstarn_complete.txt" ]] || [[ ! -s "${processed}/${project}/${waiting_sample}/Assembly/${waiting_sample}_scaffolds_trimmed.fasta" ]]; then
+		echo "${item} is complete normal"
+		# Check if plasmid csstar is complete also and wait a total of 30 minutes for all samples to be checked
+		if [[ -f "${main_dir}/complete/${waiting_sample}_csstarp_complete.txt" ]] || [[ ! -s "${processed}/${project}/${waiting_sample}/plasmidAssembly/${waiting_sample}_plasmid_scaffolds_trimmed.fasta" ]]; then
+			while :
+			do
+					if [[ ${ptimer} -gt 1800 ]]; then
+						echo "Timer exceeded limit of 1800 seconds = 30 minutes"
+						exit 1
+					fi
+					if [[ -f "${main_dir}/complete/${waiting_sample}_csstarp_complete.txt" ]]; then
+						echo "${item} is complete plasmid"
+						break
+					else
+						timer=$(( ptimer + 5 ))
+						echo "sleeping for 5 seconds, so far slept for ${ptimer}"
+						sleep 5
+					fi
+			done
+		fi
+	else
+		# Check every 5 seconds to see if the sample has completed normal csstar analysis
+		while :
+		do
+				if [[ ${timer} -gt 3600 ]]; then
+					echo "Timer exceeded limit of 3600 seconds = 60 minutes"
+					exit 1
+				fi
+				if [[ -f "${main_dir}/complete/${waiting_sample}_csstarn_complete.txt" ]]; then
+					echo "${item} is complete"
+					break
+				else
+					timer=$(( timer + 5 ))
+					echo "sleeping for 5 seconds, so far slept for ${timer}"
+					sleep 5
+				fi
+		done
+	fi
+done
+
+
 
 echo "All isolates completed"
 exit 0
