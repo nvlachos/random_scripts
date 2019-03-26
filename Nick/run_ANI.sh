1b10c780d2347dc079454793a2e310075405d085
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-26 10:59:28 -0400

    liudfdgh

--- Nick/run_ANI.sh
+++ Nick/run_ANI.sh
@@ -292,7 +292,8 @@ else
 	#Extracts the accession number from the definition line
 	accession=$(echo "${def_array[2]}" | cut -d' ' -f1  | cut -d'>' -f2)
 	#Looks up the NCBI genus species from the accession number
-	if [[ "${accession}" = "No_Accession_Number" ]]; then
+	echo "Ass-${accession}"
+	if [[ "${accession}" == "No_Accession_Number" ]]; then
 		best_organism_guess=$(echo "${def_array[2]}" | cut -d' ' -f2,3)
 	else
 		best_organism_guess=$(python "${shareScript}/entrez_get_taxon_from_accession.py" "${accession}" "${me}")
