debe751980ac29c8099b13bdddfe7828853eec0c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-28 09:39:48 -0400

    Remade abl template

--- Nick/act_by_list_template.sh
+++ Nick/act_by_list_template.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
-#$ -o abl_template.out
-#$ -e abl-template.err
-#$ -N abl-temp
+#$ -o abl-ARC.out
+#$ -e abl-ARC.err
+#$ -N abl-AR_checker
 #$ -cwd
 #$ -q all.q
 
@@ -12,7 +12,7 @@
 . "${mod_changers}/pipeline_mods"
 
 #
-# Usage ./act_by_list_AR_completion_check.sh path_to_list ResGANNOT_identifier(YYYYMMDD)
+# Usage ./act_by_list_AR_completion_check.sh path_to_list path_for_output_file
 #
 
 # Checks for proper argumentation
@@ -21,29 +21,36 @@ if [[ $# -eq 0 ]]; then
 	exit 1
 # Shows a brief uasge/help section if -h option used as first argument
 elif [[ "$1" = "-h" ]]; then
-	echo "Usage is ./act_by_list_AR_completion_check.sh path_to_list_file ResGANNOT_Identifier(YYYYMMDD)"
+	echo "Usage is ./act_by_list_AR_completion_check.sh path_to_list_file ResGANNOT_Identifier(YYYYMMDD) path_for_output_file"
 	exit 0
+elif [[ -z "${2}" ]]; then
+	echo  "No output file input, exiting..."
+	exit 1
 fi
 
 # Loop through and act on each sample name in the passed/provided list
 counter=0
+> ${2}
 while IFS= read -r var; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
-	if [[ ! -s ${processed}/${project}/${sample_name}/Assembly/${sample_name}_scaffolds_trimmed.fasta ]]; then
-		identifier=$(head -n1 ${processed}/${project}/${sample_name}/Assembly/${sample_name}_scaffolds_trimmed.fasta | cut -d'_' -f1)
-		if [[ "${identifier}" = ">NODE" ]]; then
-			echo "${counter} - ${project}/${sample_name}"
-		elif [[ "${identifier}" = ">${sample_name}" ]]; then
-			echo "${counter}"
-		else
-			echo "${counter} - ${project}/${sample_name} - I DONT KNOW - ${identifier}"
-		fi
+	echo "${counter}"
+	if [[ -f "${processed}/${project}/${sample}/MLST/${sample}_abaumannii.mlst" ]]; then
+		info=$(tail -n1 "${processed}/${project}/${sample}/MLST/${sample}_abaumannii.mlst")
+	elif [[ -f "${processed}/${project}/${sample}/MLST/${sample}_ecoli_2.mlst" ]]; then
+		info=$(tail -n1 )
 	fi
-	counter=$(( counter + 1 ))
+	for i in {3..10}; do
+		if [[ "${i}" == *","* ]] || [[ "${i}" == *"/"* ]]; then
+			echo "${project}/${sample}	dual!!!" >> "${2}"
+			break
+		fi
+	done
+	echo "${project}/${sample}	unknown?!" >> "${2}"
 done < "${1}"
+
 echo "All isolates completed"
 global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
 #Script exited gracefully (unless something else inside failed)
-printf "%s %s" "Act_by_list.sh has completed check for AR completion " "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
+printf "%s %s" "Act_by_list.sh has completed check of snd MLSTs" "${global_end_time}" | mail -s "act_by_list 2MLST complete" nvx4@cdc.gov
 exit 0
