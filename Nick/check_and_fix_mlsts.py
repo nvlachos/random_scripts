21d61b84c01af4ee6b4ea675351d46d6926c5ece
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-03 14:26:46 -0400

    xsrwybx

--- Nick/check_and_fix_mlsts.py
+++ Nick/check_and_fix_mlsts.py
@@ -38,9 +38,9 @@ def do_MLST_check(input_MLST_file, MLST_filetype, db_filename):
 				print("Unknown size "+str(list_size)+" of allele_list")
 			schemes=(list(schemes))
 			counter=0
-			for scheme in range(0, len(schemes)+1):
-				print(scheme[counter])
-				scheme[counter]=np.asarray(scheme)
+			for profile_index in range(0, len(schemes)+1):
+				print(schemes[profile_index])
+				schemes[profile_index]=np.asarray(schemes[profile_index])
 			if len(schemes) == 0:
 				print("No schemes found???")
 			elif len(schemes) == 1:
