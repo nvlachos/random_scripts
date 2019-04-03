1a8a7b135778db9d5b7c4e8d10633724461f5afa
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-03 11:38:34 -0400

    sfrgn

--- Nick/check_and_fix_mlsts.py
+++ Nick/check_and_fix_mlsts.py
@@ -30,7 +30,7 @@ def do_MLST_check(input_MLST_file, MLST_filetype):
 				schemes = it.product(allele_list[0], allele_list[1], allele_list[2], allele_list[3], allele_list[4], allele_list[5], allele_list[6])
 			elif allele_count == 8:
 				schemes = it.product(allele_list[0], allele_list[1], allele_list[2], allele_list[3], allele_list[4], allele_list[5], allele_list[6], allele_list[7])
-			else
+			else:
 				print("Unknown size of allele_list")
 		else:
 			print("Scheme is undefined")
