3122e8ffdb13f298e111b0ea34e3e40da4ba8a4c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-09 16:24:35 -0400

    xdertyjn

--- Nick/check_and_fix_mlsts.py
+++ Nick/check_and_fix_mlsts.py
@@ -99,7 +99,7 @@ def get_type(list_of_profiles, list_of_allele_names, DB_file):
 					print(list_of_profiles[0])
 					if current_profile == list_of_profiles[index]:
 						print("Match-"+str(db_items[0])
-						profiles[index]=db_items[0])
+						profiles[index] = db_items[0])
 						break
 				print("Profiles:", profiles)
 
