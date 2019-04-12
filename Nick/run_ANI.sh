0d4feb6111f68aa2901febea3eef4f2e13c4aae7
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-12 13:58:28 -0400

    Fixed qsub option errors

--- Nick/run_ANI.sh
+++ Nick/run_ANI.sh
@@ -11,7 +11,7 @@ if [[ ! -f "./config.sh" ]]; then
 	cp config_template.sh config.sh
 fi
 . ./config.sh
-#${mod_changers}/list_modules.sh
+# ${mod_changers}/list_modules.sh
 
 #
 # Script to calculate the average nucleotide identity of a sample to numerous other samples from the same genus (genus dependent)
