c7a557fa55404050be5487f8b8babc08800b47f7
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-15 14:31:05 -0400

    better pointers

--- Nick/abl_mass_qsub_ANI_contigs.sh
+++ Nick/abl_mass_qsub_ANI_contigs.sh
@@ -71,13 +71,10 @@ counter=0
 max_subs=${2}
 
 # Set script directory
-main_dir="${3}/ANI_contigs_subs"
+main_dir="${3}/ANIm_contigs_subs"
 cp ./config.sh ${main_dir}
-if [[ ! -d "${3}/ANI_contigs_subs" ]]; then
-	mkdir "${3}/ANI_contigs_subs"
-	mkdir "${3}/ANI_contigs_subs/complete"
-elif [[ ! -d "${3}/ANI_contigs_subs/complete" ]]; then
-	mkdir "${3}/ANI_contigs_subs/complete"
+if [[ ! -d "${3}/ANIm_contigs_subs/complete" ]]; then
+	mkdir -p "${3}/ANIm_contigs_subs/complete"
 fi
 
 start_time=$(date "+%m-%d-%Y_at_%Hh_%Mm_%Ss")
@@ -121,7 +118,7 @@ while [ ${counter} -lt ${arr_size} ] ; do
 		# Check if counter is below max submission limit
 	 	if [[ ${counter} -lt ${max_subs} ]]; then
 			# Check if old data exists, skip if so
-			if [[ -s "${processed}/${project}/${sample}/Assembly/${sample}_ANI_contigs_trimmed.fasta" ]]; then
+			if [[ -s "${processed}/${project}/${sample}/Contig_check/${sample}_ANI_contigs_trimmed.fasta" ]]; then
 				if [[ ! -f "${processed}/${sample}/Contig_check/ANI/best_ANIm_contigs_hits_ordered(${sample}_vs_${genus,})" ]]; then
 					echo  "Index is below max submissions, submitting"
 					echo "Going to make ${main_dir}/anic_${sample}_${start_time}.sh"
@@ -167,7 +164,7 @@ while [ ${counter} -lt ${arr_size} ] ; do
 			# Check if "waiting" sample is complete
 			if [[ -f "${main_dir}/complete/${waiting_sample}_anic_complete.txt" ]]; then
 				# Check if an assembly exists to run ANI on
-				if [[ -s "${processed}/${project}/${sample}/Assembly/${sample}_ANI_contigs_trimmed.fasta" ]]; then
+				if [[ -s "${processed}/${project}/${sample}/Contig_check/${sample}_ANI_contigs_trimmed.fasta" ]]; then
 					# Check if old data exists, skip if so
 					if [[ ! -f "${processed}/${project}/${sample}/ANI/best_ANI_hits_ordered(${sample}_vs_${genus,})" ]]; then
 						echo  "${waiting_sample}(${waiting_for_index}) is not complete, submitting ${sample} ($counter)"
