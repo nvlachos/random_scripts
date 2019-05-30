b3ffe0cc1a2eb93bc0e589d5dcaa6e5f83f4df58
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-30 13:26:13 -0400

    wrtcg

--- Nick/act_by_list_template.sh
+++ Nick/act_by_list_template.sh
@@ -42,7 +42,7 @@ while IFS= read -r var; do
 		mlst_line=$(head -n1 ${processed}/${project}/${sample_name}/MLST/${sample_name}.mlst)
 		IFS='	' read -r -a mlst_file_array <<< "$mlst_line"
 		orig="${mlst_file_array[2]}"
-		echo "${orig}"
+		echo "standard-${orig}"
 		if [[ "${mlst_file_array[2]}" == *","* ]]; then
 			mlst_file_array[2]=${mlst_file_array[2]/,/\/}
 			change=1
@@ -63,7 +63,7 @@ while IFS= read -r var; do
 		mlst_line=$(head -n1 ${processed}/${project}/${sample_name}/MLST/${sample_name}_abaumannii.mlst)
 		IFS='	' read -r -a mlst_file_array <<< "$mlst_line"
 		orig="${mlst_file_array[2]}"
-		echo "${orig}"
+		echo "ab-${orig}"
 		if [[ "${mlst_file_array[2]}" == *","* ]]; then
 			mlst_file_array[2]=${mlst_file_array[2]/,/\/}
 			change=1
@@ -84,7 +84,7 @@ while IFS= read -r var; do
 		mlst_line=$(head -n1 ${processed}/${project}/${sample_name}/MLST/${sample_name}_ecoli_2.mlst)
 		IFS='	' read -r -a mlst_file_array <<< "$mlst_line"
 		orig="${mlst_file_array[2]}"
-		echo "${orig}"
+		echo "ec-${orig}"
 		if [[ "${mlst_file_array[2]}" == *","* ]]; then
 			mlst_file_array[2]=${mlst_file_array[2]/,\/}
 			change=1
