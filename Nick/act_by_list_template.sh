b9f929486ceaa31b721137ba98340bbbd0b669b7
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-30 12:49:44 -0400

    fixed variable name

--- Nick/act_by_list_template.sh
+++ Nick/act_by_list_template.sh
@@ -35,19 +35,19 @@ while IFS= read -r var; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
 	#sed -i 's/281,1839/281\/1839/g' "${processed}/${project}/${sample_name}/MLST/${sample_name}_abaumannii.mlst"
-	if [[ -f ${processed}/${project}/${sample_name}/MLST/${sample}.mlst ]]; then
+	if [[ -f ${processed}/${project}/${sample_name}/MLST/${sample_name}.mlst ]]; then
 		mlst_line=$(head -n1 ${processed}/${project}/${sample_name}/MLST/${sample}.mlst)
 		IFS='	' read -r -a mlst_file_array <<< "$mlst_line"
 		echo "Test original: ${mlst_file_array[2]}"
 
 	fi
-	if [[ -f ${processed}/${project}/${sample_name}/MLST/${sample}_abaumannii.mlst ]]; then
+	if [[ -f ${processed}/${project}/${sample_name}/MLST/${sample_name}_abaumannii.mlst ]]; then
 		mlst_line=$(head -n1 ${processed}/${project}/${sample_name}/MLST/${sample}_abaumannii.mlst)
 		IFS='	' read -r -a mlst_file_array <<< "$mlst_line"
 		echo "Test abaumannii: ${mlst_file_array[2]}"
 
 	fi
-	if [[ -f ${processed}/${project}/${sample_name}/MLST/${sample}_ecoli_2.mlst ]]; then
+	if [[ -f ${processed}/${project}/${sample_name}/MLST/${sample_name}_ecoli_2.mlst ]]; then
 		mlst_line=$(head -n1 ${processed}/${project}/${sample_name}/MLST/${sample}_ecoli_2.mlst)
 		IFS='	' read -r -a mlst_file_array <<< "$mlst_line"
 		echo "Test ecoli: ${mlst_file_array[2]}"
