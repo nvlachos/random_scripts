4a9cf05fd89d11d9837c0925dea870fbcd7350c4
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-30 11:17:54 -0400

    reciproc

---
+++ Nick/module_changers/perl_5123_to_5221.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+#$ -o advance_perl_5221.out
+#$ -e advance_perl_5221.err
+#$ -N advance_perl_5221
+#$ -cwd
+#$ -q short.q
+
+# Unloads the perl 5.12.3 module and replaces it with the 5.22.1 version, which all other modules function on
+# Wouldnt unload normally so had to make this file...sorry
+module unload perl/5.12.3
+module load perl/5.22.1
