abf17b3c4a1e08ef65e08c3b0842576a7cec3595
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-06-19 11:00:14 -0400

    Added amq_plasflow and more complete AR consolidater

---
+++ Nick/abl_concat_AR_files.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -l
+
+#$ -o abl-ARC.out
+#$ -e abl-ARC.err
+#$ -N abl-plasCATTER
+#$ -cwd
+#$ -q short.q
+
+#Import the config file with shortcuts and settings
+. ./config.sh
+#Import the module file that loads all necessary mods
+. "${mod_changers}/pipeline_mods"
+
+#
+# Usage ./act_by_list_concat_csstar_plasFlow_files.sh path_to_list path_for_output_file
+#
+
+number='^[0-9]+$'
+
+# Checks for proper argumentation
+if [[ $# -eq 0 ]]; then
+	echo "No argument supplied to $0, exiting"
+	exit 1
+# Shows a brief uasge/help section if -h option used as first argument
+elif [[ "$1" = "-h" ]]; then
+	echo "Usage is ./act_by_list_concat_csstar_plasFlow_files.sh path_to_list_file ResGANNOT_Identifier(YYYYMMDD) path_for_output_file"
+	exit 0
+elif ! [[ ${2} =~ $number ]] || [[ -z "${2}" ]]; then
+	echo "${2} is not a number or is empty. Please input correct date for ResGANNOT DB...exiting"
+	exit 2
+elif [[ ! -f "${3}" ]]; then
+	echo "${3} does not exist"
+	outdir=$(dirname "${3}")
+	if [[ ! -d "${outdir}" ]]; then
+		echo "${outdir} does not exist"
+		mkdir -p "${outdir}"
+	fi
+	>${3}
+fi
+
+# Loop through and act on each sample name in the passed/provided list
+counter=0
+while IFS= read -r var || [ -n "$var" ]; do
+	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
+	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
+	#echo "${counter} - ${processed}/${project}/${sample_name}/MLST/"
+	if [[ -f "${processed}/${project}/${sample_name}/c-sstar_plasFlow/${sample_name}.ResGANNOT_${2}.gapped_40_sstar_summary.txt" ]]; then
+		cat "${processed}/${project}/${sample_name}/c-sstar_plasFlow/${sample_name}.ResGANNOT_${2}.gapped_40_sstar_summary.txt" >> ${3}
+	else
+		echo "${project}/${sample_name} does not have c-sstar_plasFlow 40 file"
+	fi
+done < "${1}"
+
+echo "All isolates completed"
+global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
+#Script exited gracefully (unless something else inside failed)
+printf "%s %s" "act_by_list_concat_csstar_plasFlow_files.sh has completed check of snd MLSTs" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
+exit 0
---
+++ Nick/abl_mass_qsub_plasFlow.sh
@@ -0,0 +1,191 @@
+#!/bin/sh -l
+
+#$ -o ablmq_template.out
+#$ -e ablmq_template.err
+#$ -N ablmq_template
+#$ -cwd
+#$ -q short.q
+
+#Import the config file with shortcuts and settings
+. ./config.sh
+#Import the module file that loads all necessary mods
+. "${mod_changers}/pipeline_mods"
+
+#
+# Usage ./abl_mass_qsub_template.sh path_to_list max_concurrent_submission output_directory_for_scripts clobberness[keep|clobber]
+#
+
+# Number regex to test max concurrent submission parametr
+number='^[0-9]+$'
+
+# Checks for proper argumentation
+if [[ $# -eq 0 ]]; then
+	echo "No argument supplied to $0, exiting"
+	exit 1
+# Shows a brief uasge/help section if -h option used as first argument
+elif [[ "$1" = "-h" ]]; then
+	echo "Usage is ./abl_mass_qsub_plasFlow.sh path_to_list_file(single sample ID per line, e.g. B8VHY/1700128 (it must include project id also)) max_submissions output_directory_for_scripts clobberness[keep|clobber]"
+	echo "Output location varies depending on which tasks are performed but will be found somewhere under ${processed}"
+	exit 0
+elif ! [[ ${2} =~ $number ]] || [[ -z "${2}" ]]; then
+	echo "${2} is not a number or is empty. Please input max number of concurrent qsub submissions...exiting"
+	exit 2
+elif [[ -z "${4}" ]]; then
+	echo "Clobberness was not input, be sure to add keep or clobber as 4th parameter...exiting"
+	exit 4
+fi
+
+# Check that clobberness is a valid option
+if [[ "${4}" != "keep" ]] && [[ "${4}" != "clobber" ]]; then
+	echo "Clobberness was not input correctly, be sure to add keep or clobber as 5th parameter...exiting"
+	exit 1
+else
+	clobberness="${4}"
+fi
+
+arr=()
+while IFS= read -r line || [ "$line" ];  do
+  arr+=("$line")
+done < ${1}
+
+arr_size="${#arr[@]}"
+echo "-${arr_size}:${arr[@]}-"
+
+if [[ -z ${2} ]]; then
+	max_subs=10
+fi
+
+# Loop through and act on each sample name in the passed/provided list
+counter=0
+max_subs=${2}
+main_dir="${3}/plasFlow_subs"
+if [[ ! -d "${3}/plasFlow_subs" ]]; then
+	mkdir "${3}/plasFlow_subs"
+	mkdir "${3}/plasFlow_subs/complete"
+elif [[ ! -d  "${3}/plasFlow_subs/complete" ]]; then
+	mkdir "${3}/plasFlow_subs/complete"
+fi
+
+while [ ${counter} -lt ${arr_size} ] ; do
+	sample=$(echo "${arr[${counter}]}" | cut -d'/' -f2)
+	project=$(echo "${arr[${counter}]}" | cut -d'/' -f1)
+	echo ${counter}
+	if [[ "${clobberness}" == "clobber" ]]; then
+		if [[ -d "${processed}/${project}/${sample}/plasmid" ]]; then
+			rm -r "${processed}/${project}/${sample}/plasmid"
+		fi
+	fi
+	if [ ${counter} -lt ${max_subs} ]; then
+		if [[ -f "${processed}/${project}/${sample_name}/plasFlow/Unicycler_assemblies/${sample_name}_uni_assembly/${sample_name}_plasmid_assembly_trimmed.fasta" ]]; then
+			echo  "Index is below max submissions, submitting"
+			echo -e "#!/bin/bash -l\n" > "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo -e "#$ -o pFlow_${sample}.out" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo -e "#$ -e pFlow_${sample}.err" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo -e "#$ -N pFlow_${sample}"   >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo -e "#$ -cwd"  >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo -e "#$ -q short.q\n"  >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			# Add all necessary modules
+			### echo -e "module load XXX" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+
+			# Can we somehow consolidate into one srst2 analysis to do MLST/AR/SEROTYPE
+			echo -e "\"${shareScript}/run_plasFlow.sh\" \"${sample}\" \"${project}\"" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_pFlow_complete.txt\"" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			cd "${main_dir}"
+			if [[ "${counter}" -lt "${last_index}" ]]; then
+				qsub "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			else
+				qsub -sync y "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			fi
+		else
+			echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_pFlow_complete.txt\"" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+			echo "${project}/${sample} already has plasFlow completed"
+		fi
+	else
+		waiting_for_index=$(( counter - max_subs ))
+		waiting_sample=$(echo "${arr[${waiting_for_index}]}" | cut -d'/' -f2)
+		timer=0
+		echo "Index is above max submissions, waiting for index ${waiting_for_index}:${waiting_sample} to complete"
+		while :
+		do
+			if [[ ${timer} -gt 1800 ]]; then
+				echo "Timer exceeded limit of 1800 seconds 30 minutes"
+				break
+			fi
+			if [ -f "${main_dir}/complete/${waiting_sample}_pFlow_complete.txt" ]; then
+				if [[ -f "${processed}/${project}/${sample_name}/plasFlow/Unicycler_assemblies/${sample_name}_uni_assembly/${sample_name}_plasmid_assembly_trimmed.fasta" ]]; then
+					echo  "Index is below max submissions, submitting"
+					echo -e "#!/bin/bash -l\n" > "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo -e "#$ -o pFlow_${sample}.out" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo -e "#$ -e pFlow_${sample}.err" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo -e "#$ -N pFlow_${sample}"   >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo -e "#$ -cwd"  >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo -e "#$ -q short.q\n"  >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					# Add all necessary modules
+					### echo -e "module load XXX" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+
+					# Can we somehow consolidate into one srst2 analysis to do MLST/AR/SEROTYPE
+					echo -e "\"${shareScript}/run_plasFlow.sh\" \"${sample}\" \"${project}\"" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_pFlow_complete.txt\"" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					cd "${main_dir}"
+					if [[ "${counter}" -lt "${last_index}" ]]; then
+						qsub "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					else
+						qsub -sync y "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					fi
+				else
+					echo -e "echo \"$(date)\" > \"${main_dir}/complete/${sample}_pFlow_complete.txt\"" >> "${main_dir}/pFlow_${sample}_${start_time}.sh"
+					echo "${project}/${sample} already has plasFlow completed"
+				fi
+				break
+			else
+				timer=$(( timer + 5 ))
+				echo "sleeping for 5 seconds, so far slept for ${timer}"
+				sleep 5
+			fi
+		done
+	fi
+	counter=$(( counter + 1 ))
+done
+
+# Check for completion of all samples
+timer=0
+for item in "${arr[@]}"; do
+	waiting_sample=$(echo "${item}" | cut -d'/' -f2)
+	if [[ -f "${main_dir}/complete/${waiting_sample}_pFlow_complete.txt" ]]; then
+		echo "${item} is complete"
+		if [[ -f "${shareScript}/pFlow_${waiting_sample}.out" ]]; then
+			mv "${shareScript}/pFlow_${waiting_sample}.out" "${main_dir}"
+		fi
+		if [[ -f "${shareScript}/pFlow_${waiting_sample}.err" ]]; then
+			mv "${shareScript}/pFlow_${waiting_sample}.err" "${main_dir}"
+		fi
+	else
+		while :
+		do
+				if [[ ${timer} -gt 3600 ]]; then
+					echo "Timer exceeded limit of 3600 seconds = 60 minutes"
+					exit 1
+				fi
+				if [[ -f "${main_dir}/complete/${waiting_sample}_pFlow_complete.txt" ]]; then
+					echo "${item} is complete"
+					if [[ -f "${shareScript}/pFlow_${waiting_sample}.out" ]]; then
+						mv "${shareScript}/pFlow_${waiting_sample}.out" "${main_dir}"
+					fi
+					if [[ -f "${shareScript}/pFlow_${waiting_sample}.err" ]]; then
+						mv "${shareScript}/pFlow_${waiting_sample}.err" "${main_dir}"
+					fi
+					break
+				else
+					timer=$(( timer + 5 ))
+					echo "sleeping for 5 seconds, so far slept for ${timer}"
+					sleep 5
+				fi
+		done
+	fi
+done
+
+echo "All isolates completed"
+global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
+#Script exited gracefully (unless something else inside failed)
+printf "%s %s" "abl_mass_qsub_template.sh has completed ${2}" "${global_end_time}" | mail -s "abl_mass_qsub complete" nvx4@cdc.gov
+exit 0
