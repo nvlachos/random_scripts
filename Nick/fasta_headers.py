9c516e8cc3af26c9530cfee3f61b75392d048113
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-09 13:24:25 -0400

    added fasta headers for abl prokka

--- Nick/fasta_headers.py
+++ Nick/fasta_headers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 '''
-Changes fasta header from:
+Changes fasta header between:
 NODE_contig#_length_length#_cov_cov#
-to
+and
 Name_contig#_length_length#
 
 
-Usage: ./fasta_headers.py -i file.fasta -o output.fasta
+Usage: ./fasta_headers.py -i file.fasta -o output.fasta [-r]
 '''
 
 from Bio import SeqIO
@@ -21,21 +21,47 @@ def parseArgs(args=None):
 	parser = argparse.ArgumentParser(description='Script to rename contigs in assemblies')
 	parser.add_argument('-i', '--input', required=True, help='input fasta filename')
 	parser.add_argument('-o', '--output', required=True, help='output filename')
+	parser.add_argument('--reverse', help='returns formatted header to original', action='store_true')
 	return parser.parse_args()
 
 args=parseArgs()
 sequences = []
-for record in SeqIO.parse(args.input,"fasta"):
-    #print(record.id)
-    name=os.path.basename(args.input).split("_")[::-1]
-    name=name[3:]
-    name='_'.join(name[::-1])
-    #print(name)
-    record.id = record.id.split("_cov")[0].replace("NODE",name)
-    #print(record.id)
-    record.description = ""
-#    print(record.description)
-#    print(record)
-    sequences.append(record)
 
-SeqIO.write(sequences, args.output, "fasta")
+if not args.reverse:
+	#print("FORWARD")
+	name=os.path.basename(args.input).split("_")[::-1]
+	#print(name)
+	name=name[3:]
+	#print(name)
+	name='_'.join(name[::-1])
+	#print(name)
+	for record in SeqIO.parse(args.input,"fasta"):
+	    #print(record.id)
+	    #print(name)
+	    record.id = record.id.split("_cov")[0].replace("NODE",name)
+	    #print(record.id)
+	    record.description = ""
+	#    print(record.description)
+	#    print(record)
+	    sequences.append(record)
+
+	SeqIO.write(sequences, args.output, "fasta")
+else:
+	#print("REVERSE")
+	name=os.path.basename(args.input).split("_")[::-1]
+	#print(name)
+	name=name[2:]
+	#print(name)
+	name='_'.join(name[::-1])
+	#print(name)
+	for record in SeqIO.parse(args.input,"fasta"):
+	    #print(record.id)
+	    #print(name)
+	    record.id = record.id.replace(name,"NODE")+"_cov_X"
+	    #print(record.id)
+	    record.description = ""
+		#print(record.description)
+		#print(record)
+	    sequences.append(record)
+
+	SeqIO.write(sequences, args.output, "fasta")
