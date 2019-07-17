1e075ab7827d012a3989287fe521658b23377465
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-17 16:14:11 -0400

    wbrvt

--- Nick/abl_template.sh
+++ Nick/abl_template.sh
@@ -29,7 +29,7 @@ fi
 while IFS= read -r var  || [ -n "$var" ]; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
-	rm "${processed}/${project}/${sample_name}/MLST/${sample_name}.mlst"
+	sed -i -e 's/ /	/g' "${processed}/${project}/${sample_name}/MLST/${sample_name}.mlst"
 done < "${1}"
 
 echo "All isolates completed"
