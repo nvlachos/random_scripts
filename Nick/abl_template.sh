e19cfb0cf23853a84439d5d2355d2284209b3a40
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-17 15:45:31 -0400

    exrthg

--- Nick/abl_template.sh
+++ Nick/abl_template.sh
@@ -29,7 +29,7 @@ fi
 while IFS= read -r var  || [ -n "$var" ]; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
-	cat "${processed}/${project}/${sample}/MLST/${sample}.mlst" >> /scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/mlst.out
+	cat "${processed}/${project}/${sample_name}/MLST/${sample_name}.mlst" >> /scicomp/groups/OID/NCEZID/DHQP/CEMB/Nick_DIR/mlst.out
 done < "${1}"
 
 echo "All isolates completed"
