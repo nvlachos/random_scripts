cdd8ac93e16f4a01989e746e023034f56f5b2ba4
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-26 09:30:40 -0400

    Doesnt need to load pipeline mods, it just checks existence of files

--- Nick/abl_full_completion_check.sh
+++ Nick/abl_full_completion_check.sh
@@ -9,7 +9,7 @@
 #Import the config file with shortcuts and settings
 . ./config.sh
 #Import the module file that loads all necessary mods
-. "${mod_changers}/pipeline_mods"
+#. "${mod_changers}/pipeline_mods"
 
 #
 # Usage ./act_by_list_AR_completion_check.sh path_to_list ResGANNOT_identifier(YYYYMMDD) path_for_output_file
