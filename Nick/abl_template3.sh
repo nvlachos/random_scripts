477a49156998acb60bfdb4818fc3d120cc4bb977
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-30 13:07:34 -0400

    ersgcf

---
+++ Nick/abl_template3.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -l
+
+#$ -o abl-template.out
+#$ -e abl-template.err
+#$ -N abl-template
+#$ -cwd
+#$ -q short.q
+
+#Import the config file with shortcuts and settings
+. ./config.sh
+#Import the module file that loads all necessary mods
+#. "${mod_changers}/pipeline_mods"
+
+#
+# Usage ./act_by_list_template.sh path_to_list path_for_output_file
+#
+
+# Checks for proper argumentation
+if [[ $# -eq 0 ]]; then
+	echo "No argument supplied to $0, exiting"
+	exit 1
+# Shows a brief uasge/help section if -h option used as first argument
+elif [[ "$1" = "-h" ]]; then
+	echo "Usage is ./act_by_list_template.sh path_for_list_file"
+	exit 0
+fi
+
+#ml BBMap/38.26 trimmomatic/0.35
+
+# Loop through and act on each sample name in the passed/provided list
+while IFS= read -r var; do
+	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
+	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
+
+	if [[ -f "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq.gz" ]]; then
+		echo "About to delete - ${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
+		rm "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
+	fi
+	if [[ -f "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq.gz" ]]; then
+		echo "About to delete - ${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
+		rm "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
+	fi
+done < "${1}"
+
+#ml -BBMap/38.26 -trimmomatic/0.35
+
+echo "All isolates completed"
+global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
+#Script exited gracefully (unless something else inside failed)
+printf "%s %s" "Act_by_list_template.sh has completed check of snd MLSTs" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
+exit 0
