1c7166341f9814cf1c10c89a2c27ef8f758ce430
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-08-02 09:33:49 -0400

    rth

--- Nick/abl_template2.sh
+++ Nick/abl_template2.sh
@@ -25,32 +25,34 @@ elif [[ "$1" = "-h" ]]; then
 	exit 0
 fi
 
-ml BBMap/38.26 trimmomatic/0.35
+#ml BBMap/38.26 trimmomatic/0.35
 
 # Loop through and act on each sample name in the passed/provided list
 while IFS= read -r var; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
-	bbduk.sh -"${bbduk_mem}" threads="${procs}" in="${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R1_001.fastq" in2="${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R2_001.fastq" out="${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R1.fsq" out2="${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R2.fsq" ref="${phiX_location}" k="${bbduk_k}" hdist="${bbduk_hdist}"
-	trimmomatic "${trim_endtype}" -"${trim_phred}" -threads "${procs}" "${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R1.fsq" "${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R2.fsq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.unpaired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.unpaired.fq" ILLUMINACLIP:"${trim_adapter_location}:${trim_seed_mismatch}:${trim_palindrome_clip_threshold}:${trim_simple_clip_threshold}:${trim_min_adapt_length}:${trim_complete_palindrome}" SLIDINGWINDOW:"${trim_window_size}:${trim_window_qual}" LEADING:"${trim_leading}" TRAILING:"${trim_trailing}" MINLEN:"${trim_min_length}"
-	rm -r "${processed}/${project}/${sample_name}/removedAdapters"
+	#bbduk.sh -"${bbduk_mem}" threads="${procs}" in="${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R1_001.fastq" in2="${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R2_001.fastq" out="${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R1.fsq" out2="${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R2.fsq" ref="${phiX_location}" k="${bbduk_k}" hdist="${bbduk_hdist}"
+	#trimmomatic "${trim_endtype}" -"${trim_phred}" -threads "${procs}" "${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R1.fsq" "${processed}/${project}/${sample_name}/removedAdapters/${sample_name}-noPhiX-R2.fsq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.unpaired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.unpaired.fq" ILLUMINACLIP:"${trim_adapter_location}:${trim_seed_mismatch}:${trim_palindrome_clip_threshold}:${trim_simple_clip_threshold}:${trim_min_adapt_length}:${trim_complete_palindrome}" SLIDINGWINDOW:"${trim_window_size}:${trim_window_qual}" LEADING:"${trim_leading}" TRAILING:"${trim_trailing}" MINLEN:"${trim_min_length}"
+	#rm -r "${processed}/${project}/${sample_name}/removedAdapters"
 	if [[ ! -f "${processed}/${project}/${sample_name}/trimmed/${sample_name}.paired.fq" ]]; then
 		cat "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq" > "${processed}/${project}/${sample_name}/trimmed/${sample_name}.paired.fq"
 	fi
+	rm "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
+	rm "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
 	if [[ ! -f "${processed}/${project}/${sample_name}/trimmed/${sample_name}.single.fq" ]]; then
 		cat "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.unpaired.fq" "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.unpaired.fq" > "${processed}/${project}/${sample_name}/trimmed/${sample_name}.single.fq"
 		rm "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.unpaired.fq"
 		rm "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.unpaired.fq"
 	fi
-	echo "About to zip - ${processed}/${project}/${sample_name}/trimmed/${sample_name}.paired.fq"
-	gzip "${processed}/${project}/${sample_name}/trimmed/${sample_name}.paired.fq"
-	echo "About to zip - ${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
-	gzip "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
-	echo "About to zip - ${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
-	gzip "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
+	#echo "About to zip - ${processed}/${project}/${sample_name}/trimmed/${sample_name}.paired.fq"
+	#gzip "${processed}/${project}/${sample_name}/trimmed/${sample_name}.paired.fq"
+	#echo "About to zip - ${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
+	#gzip "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq"
+	#echo "About to zip - ${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
+	#gzip "${processed}/${project}/${sample_name}/trimmed/${sample_name}_R2_001.paired.fq"
 done < "${1}"
 
-ml -BBMap/38.26 -trimmomatic/0.35
+#ml -BBMap/38.26 -trimmomatic/0.35
 
 echo "All isolates completed"
 global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
