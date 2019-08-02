33a11c66082942fcea4b22f05365111450b0d2bc
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-08-02 14:33:02 -0400

    ujghh

--- Nick/check_and_fix_MLST.sh
+++ Nick/check_and_fix_MLST.sh
@@ -38,7 +38,7 @@ while IFS= read -r line_in; do
 	sample=$(echo "${line_in}" | cut -d'/' -f2)
 	project=$(echo "${line_in}" | cut -d'/' -f1)
 	# Check main automated mlst file first
-	for mlst_file in ${processed}/${project}/${sample}/MLST/*; do
+	for mlst_file in "${processed}/${project}/${sample}/MLST/"*; do
 		echo "checking ${mlst_file}"
 		if [[ "${mlst_file}" == *".mlst" ]]; then
 			if [[ "${mlst_file}" == *"srst2"* ]]; then
