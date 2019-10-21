19eba10719c8493c6dde476c34e66e8591901464
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-10-21 10:16:25 -0400

    Fixed if error

--- Nick/abl_mass_qsub_MLST.sh
+++ Nick/abl_mass_qsub_MLST.sh
@@ -33,7 +33,7 @@ if [[ $# -eq 0 ]]; then
 elif [[ "$1" = "-h" ]]; then
 	echo "Usage is ./abl_mass_qsub_MLST.sh path_to_list_file(single sample ID per line, e.g. B8VHY/1700128 (it must include project id also)) max_concurrent_submissions output_directory_for_scripts clobberness[keep|clobber]"
 	exit 1
-
+fi
 
 # Check that clobberness is a valid option
 if [[ "${4}" != "keep" ]] && [[ "${4}" != "clobber" ]]; then
