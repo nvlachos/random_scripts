3aebbdefa3e35307301ef233a900e8452585569b
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-10-24 13:33:35 -0400

    Supposed to be rm not gzip

--- Nick/abl_template3.sh
+++ Nick/abl_template3.sh
@@ -30,7 +30,7 @@ while IFS= read -r var; do
 	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
 	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
 
-	gzip ${processed}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq
+	rm ${processed}/${sample_name}/trimmed/${sample_name}_R1_001.paired.fq
 
 	echo "Finished with ${project}/${sample_name}"
 
