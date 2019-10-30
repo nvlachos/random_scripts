5a0b374d76a7522dd45c84ecdc0991028da48b7c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-10-30 13:27:41 -0400

    ezrg

---
+++ Nick/fix_list_remove_unzipped_raw_reads.sh
@@ -0,0 +1,64 @@
+#!/bin/sh -l
+
+#$ -o abl-template5.out
+#$ -e abl-template5.err
+#$ -N abl-template5
+#$ -cwd
+#$ -q short.q
+
+#Import the config file with shortcuts and settings
+. ./config.sh
+#Import the module file that loads all necessary mods
+#. "${mod_changers}/pipeline_mods"
+
+#
+# Usage ./act_by_list_template.sh path_to_list path_for_output_file
+#
+
+# Checks for proper argumentation
+if [[ $# -eq 0 ]]; then
+	echo "No argument supplied to $0, exiting"
+	exit 1
+# Shows a brief uasge/help section if -h option used as first argument
+elif [[ "$1" = "-h" ]]; then
+	echo "Usage is ./act_by_list_template.sh path_for_list_file"
+	exit 0
+fi
+
+# Loop through and act on each sample name in the passed/provided list
+while IFS= read -r var; do
+	sample_name=$(echo "${var}" | cut -d'/' -f2 | tr -d '[:space:]')
+	project=$(echo "${var}" | cut -d'/' -f1 | tr -d '[:space:]')
+
+	if [[ -f "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R1_001.fastq" ]]; then
+		if [[ ! -f "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R1_001.fastq" ]]; then
+			echo "1-1"
+			gzip "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R1_001.fastq"
+		else
+			echo "1-2"
+			rm "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R1_001.fastq"
+		fi
+	else
+		echo "1-0"
+	fi
+	if [[ -f "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R2_001.fastq" ]]; then
+		if [[ ! -f "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R2_001.fastq" ]]; then
+			echo "2-1"
+			gzip "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R2_001.fastq"
+		else
+			echo "2-2"
+			rm "${processed}/${project}/${sample_name}/FASTQs/${sample_name}_R2_001.fastq"
+		fi
+	else
+		echo "2-0"
+	fi
+
+	echo "Finished with ${project}/${sample_name}"
+
+done < "${1}"
+
+echo "All isolates completed"
+global_end_time=$(date "+%m-%d-%Y @ %Hh_%Mm_%Ss")
+#Script exited gracefully (unless something else inside failed)
+printf "%s %s" "Act_by_list_template.sh has completed check of snd MLSTs" "${global_end_time}" | mail -s "act_by_list complete" nvx4@cdc.gov
+exit 0
