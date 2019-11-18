79be123f487fbb2164efa5c42c49e73901733c9a
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-11-18 09:21:26 -0500

    rtuh

--- Nick/abl_mass_qsub_sample_cleaner.sh
+++ Nick/abl_mass_qsub_sample_cleaner.sh
@@ -72,7 +72,7 @@ time_run=$(date "+%m-%d-%Y_at_%Hh_%Mm_%Ss")
 
 # Creates and submits qsub scripts to perform summaries of each run in the list
 while [ ${counter} -lt ${arr_size} ] ; do
-	sample=$(echo "${arr[${counter}]}" | cut -d'/' -f2)
+	sample_name=$(echo "${arr[${counter}]}" | cut -d'/' -f2)
 	project=$(echo "${arr[${counter}]}" | cut -d'/' -f1)
 	if [[ ${counter} -lt ${max_subs} ]]; then
 		echo  "Index is below max submissions, submitting"
